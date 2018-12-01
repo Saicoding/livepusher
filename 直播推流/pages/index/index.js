5f8df7310b6afe053433992f6b18573dca3db2fa
Saicoding <417353147@qq.com>
2018-12-01 16:18:22 +0800

    完善可见BUG

--- 直播推流/pages/index/index.js
+++ 直播推流/pages/index/index.js
@@ -1,7 +1,7 @@
 // pages/index/index.js
 let API_URL = "https://xcx2.chinaplat.com/laoxie/";
 let app = getApp();
-let hasInterval = false;//默认没有计时器
+let hasInterval = false; //默认没有计时器
 
 Page({
 
@@ -9,16 +9,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    fairLevel:0,
-    whiteningLevel:0,
+    fairLevel: 0,
+    whiteningLevel: 0,
     show: true,
-    showSet:false,
-    clarity:"RTC",
-    isPlaying:false,
-    isPauseing:false,//是否暂停中
-    isVoiceOn:true,//是否开启声音，默认开启
-    nums:0,//直播间人数
-    showModal:false,
+    showSet: false,
+    clarity: "RTC",
+    isPlaying: false,
+    isPauseing: false, //是否暂停中
+    isVoiceOn: false, //是否开启声音，默认开启
+    nums: 0, //直播间人数
   },
 
   /**
@@ -26,23 +25,31 @@ Page({
    */
   onLoad: function(options) {
 
+    wx.setKeepScreenOn({
+      keepScreenOn: true
+    });
+
+    this.setData({
+      videoBitrate: 0,
+      audioBitrate: 0,
+      videoFPS: 0,
+      videoGOP: 0,
+      netSpeed: 0
+    })
   },
 
   /**
    * 直播状态改变时
    */
-  stateChange:function(e){
+  stateChange: function(e) {
     let code = e.detail.code;
-    this.setData({
-      showModal:true
-    })
 
-    switch(code){
+    switch (code) {
       case 1001:
         this.setData({
-          prompt:"已经连接推流服务器"
+          prompt: "已经连接推流服务器"
         })
-      break;
+        break;
       case 1002:
         this.setData({
           prompt: "已经与服务器握手完毕,开始推流"
@@ -81,32 +88,38 @@ Page({
         break;
       case 1006:
       case 1005:
+      case 1008:
+      case 3005:
+      case 1007:
         this.setData({
           prompt: "",
-          showModal:false,
         })
         break;
-
-        case -1307:
+      case -1307:
         this.setData({
           prompt: "网络断连"
         })
-        default:
+        break;
+      default:
         this.setData({
           prompt: code
         })
         break;
     }
   },
-  netstatus:function(e){
+
+  /**
+   * 网络状态
+   */
+  netstatus: function(e) {
     let info = e.detail.info;
-    let videoBitrate = info.videoBitrate;//当前视频编/码器输出的比特率，单位 kbps
-    let audioBitrate = info.audioBitrate;//当前音频编/码器输出的比特率，单位 kbps
-    let videoFPS = info.videoFPS;//当前视频帧率
-    let videoGOP = info.videoGOP;//当前视频 GOP,也就是每两个关键帧(I帧)间隔时长，单位 s
-    let netSpeed = info.netSpeed;//当前的发送/接收速度
-    let netJitter = info.netJitter;//网络抖动情况，抖动越大，网络越不稳定
-    
+    let videoBitrate = info.videoBitrate; //当前视频编/码器输出的比特率，单位 kbps
+    let audioBitrate = info.audioBitrate; //当前音频编/码器输出的比特率，单位 kbps
+    let videoFPS = info.videoFPS; //当前视频帧率
+    let videoGOP = info.videoGOP; //当前视频 GOP,也就是每两个关键帧(I帧)间隔时长，单位 s
+    let netSpeed = info.netSpeed; //当前的发送/接收速度
+    let netJitter = info.netJitter; //网络抖动情况，抖动越大，网络越不稳定
+
     this.setData({
       videoBitrate: videoBitrate,
       audioBitrate: audioBitrate,
@@ -120,7 +133,7 @@ Page({
   /**
    * 发生错误时
    */
-  error:function(e){
+  error: function(e) {
     let message = e.detailerrMsg;
     let errorCode = e.detail.errorCode
     this.setData({
@@ -130,18 +143,18 @@ Page({
   },
 
   /** 
-     * 点击视频显示控制面板
-     */
-  toogleShow: function (e) {
+   * 点击视频显示控制面板
+   */
+  toogleShow: function(e) {
     let self = this;
 
     let show = self.data.show;
     let showSet = self.data.showSet;
 
-    if(showSet){
+    if (showSet) {
       show = true;
       showSet = false;
-    }else{
+    } else {
       show = true;
     }
 
@@ -154,14 +167,14 @@ Page({
   /**
    * 切换显示视频设置
    */
-  myset:function(){
+  myset: function() {
     let self = this;
     let showSet = self.data.showSet;
 
 
     this.setData({
       showSet: true,
-      show:false
+      show: false,
     })
   },
 
@@ -169,18 +182,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-    this.ctx = wx.createLivePusherContext('pusher',this);
-
-    this.ctx.start({
-      success: res => {
-        self.setData({
-          isPlaying: true
-        })
-      },
-      fail: res => {
-        console.log('start fail')
-      }
-    })
+    this.ctx = wx.createLivePusherContext('pusher', this);
 
     let self = this;
     wx.getSystemInfo({ //得到窗口高度,这里必须要用到异步,而且要等到窗口bar显示后再去获取,所以要在onReady周期函数中使用获取窗口高度方法
@@ -207,13 +209,13 @@ Page({
       let k = user.k;
       let zcode = user.zcode;
 
-      let id = zcode+k
+      let id = zcode + k
 
       self.setData({
         id: id
       })
 
-      if(!hasInterval){
+      if (!hasInterval) {
         hasInterval = true;
         let LoginRandom = user.Login_random;
         let zcode = user.zcode;
@@ -223,7 +225,7 @@ Page({
             let nums = res.data.data[0].nums;
 
             self.setData({
-              nums:nums
+              nums: nums
             })
           })
         }, 2000)
@@ -240,38 +242,39 @@ Page({
   /**
    * 改变清晰度
    */
-  changeClarity:function(e){
-    let clarity =  e.currentTarget.dataset.clarity;
+  changeClarity: function(e) {
+    let clarity = e.currentTarget.dataset.clarity;
     this.setData({
-      clarity:clarity
+      clarity: clarity
     })
   },
 
   /**
    * 开始直播或者恢复直播
    */
-  
-  start:function(){
+  start: function() {
     let self = this;
-    let isPauseing = self.data.isPauseing;//是否是暂停中
+    let isPauseing = self.data.isPauseing; //是否是暂停中
 
-    if (isPauseing){//如果暂停
-      this.ctx.resume({//恢复直播
+    if (isPauseing) { //如果暂停
+      this.ctx.resume({ //恢复直播
         success: res => {
           self.setData({
             isPlaying: true,
-            isPauseing:false
+            isPauseing: false,
+            isVoiceOn: false,
           })
         },
         fail: res => {
           console.log('start fail')
         }
       })
-    }else{
+    } else {
       this.ctx.start({
         success: res => {
           self.setData({
-            isPlaying: true
+            isPlaying: true,
+            isVoiceOn: false,
           })
         },
         fail: res => {
@@ -281,16 +284,22 @@ Page({
     }
   },
 
-   /**
-    * 暂停直播
-    */
-  pause:function(){
+  /**
+   * 暂停直播
+   */
+  pause: function() {
     let self = this;
     this.ctx.pause({
-      success:res =>{
+      success: res => {
         self.setData({
+          isVoiceOn: true,
           isPlaying: false,
-          isPauseing:true
+          isPauseing: true,
+          videoBitrate: 0,
+          audioBitrate: 0,
+          videoFPS: 0,
+          videoGOP: 0,
+          netSpeed: 0
         })
       }
     })
@@ -299,12 +308,18 @@ Page({
   /**
    * 停止直播
    */
-  stop: function () {
+  stop: function() {
     let self = this;
     this.ctx.stop({
       success: res => {
         self.setData({
-          isPlaying: false
+          isPlaying: false,
+          isVoiceOn: true,
+          videoBitrate: 0,
+          audioBitrate: 0,
+          videoFPS: 0,
+          videoGOP: 0,
+          netSpeed: 0
         })
       }
     })
@@ -313,7 +328,7 @@ Page({
   /**
    * 退出登录
    */
-  loginOut:function(){
+  loginOut: function() {
     wx.showModal({
       content: '您确定要退出登录吗？',
       confirmColor: '#0097f5',
@@ -324,7 +339,7 @@ Page({
 
           wx.removeStorage({
             key: 'user',
-            success: function (res) {
+            success: function(res) {
               let url = encodeURIComponent('/pages/index/index');
               wx.navigateTo({
                 url: '/pages/login1/login1?url=' + url + '&ifGoPage=false',
@@ -338,18 +353,18 @@ Page({
   /**
    * 设置美颜
    */
-  setFair:function(e){
+  setFair: function(e) {
     let fair = e.currentTarget.dataset.fair;
     let fairLevel = this.data.fairLevel;
-    
-    if (fair == "0" && fairLevel >0){//点了减号
+
+    if (fair == "0" && fairLevel > 0) { //点了减号
       fairLevel--;
       this.setData({
         fairLevel: fairLevel
       })
     }
 
-    if(fair == "1" && fairLevel < 9){
+    if (fair == "1" && fairLevel < 9) {
       fairLevel++;
       this.setData({
         fairLevel: fairLevel
@@ -360,11 +375,11 @@ Page({
   /**
    * 设置美白
    */
-  setWhitening:function(e){
+  setWhitening: function(e) {
     let whitening = e.currentTarget.dataset.whitening;
     let whiteningLevel = this.data.whiteningLevel;
 
-    if (whitening == "0" && whiteningLevel > 0) {//点了减号
+    if (whitening == "0" && whiteningLevel > 0) { //点了减号
       whiteningLevel--;
       this.setData({
         whiteningLevel: whiteningLevel
@@ -381,21 +396,20 @@ Page({
   /**
    * 设置是否静音
    */
-  setVoice:function(){
+  setVoice: function() {
     this.setData({
-      isVoiceOn:!this.data.isVoiceOn
+      isVoiceOn: !this.data.isVoiceOn
     })
   },
 
   /**
    * 设置前后摄像头
    */
-  setCamera:function(){
+  setCamera: function() {
     let self = this;
     this.ctx.switchCamera({
-      success:function(){
-      }
+      success: function() {}
     })
   }
-  
+
 })
